a24d56b07abe3ef5f79c9911060dd5adab183211
Muneeb Mukhtar <muneebkt@gmail.com>
2024-03-21 23:39:16 +0530

    initial commit

---
+++ js/yummy.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * yummy behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.yummy = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
