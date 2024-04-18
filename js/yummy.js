6486f439105338f0dc180b34d1429df9e0e11ee9
Muneeb Mukhtar <muneebkt@gmail.com>
2024-04-18 11:19:30 +0530

    Updating theme to github

--- js/yummy.js
+++ js/yummy.js
@@ -2,16 +2,21 @@
  * @file
  * yummy behaviors.
  */
-(function (Drupal) {
+(function (Drupal, $) {
 
   'use strict';
 
   Drupal.behaviors.yummy = {
-    attach (context, settings) {
-
-      console.log('It works!');
-
+    attach(context, settings) {
+      //Change mobile menu book table text
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $(".btn-book-a-table").text("Book table");
+        $(".btn-book-a-table").css({
+          'margin-left': '120px',
+          'position': 'absolute'
+        });
+      }
     }
   };
 
-} (Drupal));
+}(Drupal, jQuery));
